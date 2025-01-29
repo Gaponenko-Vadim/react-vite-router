22e428db0f13539c56e28c0c9901ee242b09b1fb
Vadim-Gaponenko <GaponenkoStrem@gmail.com>
2025-01-29 18:43:38 +0300

    Добавил Section

--- src/containers/Section/index.tsx
+++ src/containers/Section/index.tsx
@@ -1,1 +1,13 @@
+import React from 'react';
+import { SectionProps } from './types';
+import styles from './styles.module.scss'; // Импортируем стили, если они нужны
 
+const Section: React.FC<SectionProps> = ({ children }) => {
+	return (
+		<section className={styles.section}>
+			<div className={styles.container}>{children}</div>
+		</section>
+	);
+};
+
+export default Section;
