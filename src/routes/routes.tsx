fc71a38bf5a1054867eb59b42d33a29d7fce6418
Vadim-Gaponenko <GaponenkoStrem@gmail.com>
2025-01-30 17:17:56 +0300

    add router

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,1 +1,41 @@
-export const routes = [{ index: true, element: <p>Hello world!</p> }];
+import MainPage from '@/pages/MainPage';
+import LoginPage from '@/pages/LoginPage';
+import RegisterPage from '@/pages/RegistrPage';
+
+export const routes = [
+	{
+		path: '/',
+		element: <MainPage />,
+	},
+	{
+		path: '/login',
+		element: (
+			<LoginPage
+				title="Login Page"
+				onSubmit={(email: string, password: string) => {
+					console.log('Login submitted:', { email, password });
+				}}
+			/>
+		),
+	},
+	{
+		path: '/register',
+		element: (
+			<RegisterPage
+				onSubmit={(
+					name: string,
+					email: string,
+					password: string,
+					confirmPassword: string
+				) => {
+					console.log('Register submitted:', {
+						name,
+						email,
+						password,
+						confirmPassword,
+					});
+				}}
+			/>
+		),
+	},
+];
